7b343adfa3625377ec068b9e45539a4e97444af7
Mohamed Hashim <102317409+mohamedhashim173@users.noreply.github.com>
2022-12-01 21:48:07 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 25, 2022
+ *      Author: Mohamed Hashim Attiya Said
+ */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+
+
+// Blinking LED
+void main(void)
+{
+	DIO_voidSetPinDirection(DIO_u8PORTA, DIO_u8PIN0, DIO_u8PIN_OUTPUT);
+
+	while(1)
+	{
+		DIO_voidSetPinValue(DIO_u8PORTA, DIO_u8PIN0, DIO_u8PIN_HIGH);
+		_delay_ms(500);
+		DIO_voidSetPinValue(DIO_u8PORTA, DIO_u8PIN0, DIO_u8PIN_LOW);
+		_delay_ms(500);
+	}
+}
+
